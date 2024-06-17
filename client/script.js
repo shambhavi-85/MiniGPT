1ace51ff35f1b19551eb1869e3fcbdb25720156b
LEGO-SUDO <nakulksingh3127@gmail.com>
2024-06-17 20:25:24 +0530

    fix
    openai req structure change

--- client/script.js
+++ client/script.js
@@ -1,46 +1,45 @@
-import bot from './assets_codex/assets/bot.svg';
-import user from './assets_codex/assets/user.svg';
+import bot from "./assets_codex/assets/bot.svg"
+import user from "./assets_codex/assets/user.svg"
 
-const form = document.querySelector('form');
-const chatContainer = document.querySelector('#chat_container');
+const form = document.querySelector("form")
+const chatContainer = document.querySelector("#chat_container")
 
-let loadInterval;
+let loadInterval
 
-function loader(element){
-  element.textContent ='';
+function loader(element) {
+  element.textContent = ""
   loadInterval = setInterval(() => {
-    element.textContent +='.';
-    if(element.textContent === '....'){
-      element.textContent= '';
+    element.textContent += "."
+    if (element.textContent === "....") {
+      element.textContent = ""
     }
-  },300)
+  }, 300)
 }
 
-function typeText(element, text){
-  let index =0;
+function typeText(element, text) {
+  let index = 0
 
   let interval = setInterval(() => {
-    if(index< text.length){
-      element.innerHTML += text.charAt(index); //character at particular index of text
-      index++;
-    }else{
-      clearInterval(interval);
+    if (index < text.length) {
+      element.innerHTML += text.charAt(index) //character at particular index of text
+      index++
+    } else {
+      clearInterval(interval)
     }
-  },20)
+  }, 20)
 }
 
-function generateUniqueId(){
-  const timestamp =Date.now();
-  const randomNumber = Math.random();
-  const hexadecimalString = randomNumber.toString(16);
+function generateUniqueId() {
+  const timestamp = Date.now()
+  const randomNumber = Math.random()
+  const hexadecimalString = randomNumber.toString(16)
 
-  return `id-${timestamp}-${hexadecimalString}`;
+  return `id-${timestamp}-${hexadecimalString}`
 }
 
-function chatStripe (isAi, value, uniqueId){
- return(
-  `
-  <div class="wrapper ${isAi && 'ai'}">
+function chatStripe(isAi, value, uniqueId) {
+  return `
+  <div class="wrapper ${isAi && "ai"}">
    <div class = "chat">
     <div class ="profile">
       <img src= "${isAi ? bot : user}"
@@ -51,56 +50,55 @@ function chatStripe (isAi, value, uniqueId){
   </div>
   
   `
- )
 }
 
-const handleSubmit = async(e) => {
-  e.preventDefault(); //will prevent default behaviour of browser restaring
+const handleSubmit = async (e) => {
+  e.preventDefault() //will prevent default behaviour of browser restaring
 
-  const data = new FormData(form);
+  const data = new FormData(form)
 
   //users chatstripe
-  chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
-  form.reset();
+  chatContainer.innerHTML += chatStripe(false, data.get("prompt"))
+  form.reset()
 
   //bots chatstripe
-  const uniqueId = generateUniqueId();
-  chatContainer.innerHTML += chatStripe(true, " ",uniqueId);
-  chatContainer.scrollTop= chatContainer.scrollHeight;
-  const messageDiv = document.getElementById(uniqueId);
+  const uniqueId = generateUniqueId()
+  chatContainer.innerHTML += chatStripe(true, " ", uniqueId)
+  chatContainer.scrollTop = chatContainer.scrollHeight
+  const messageDiv = document.getElementById(uniqueId)
 
-  loader(messageDiv);
+  loader(messageDiv)
 
   //fetch data from server -> bot's response
 
-  const response = await fetch('http://localhost:5173', {
-    method : 'POST',
+  const response = await fetch("http://localhost:8000", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
-    body:JSON.stringify({
-      prompt: data.get('prompt')
-    })
+    body: JSON.stringify({
+      prompt: data.get("prompt"),
+    }),
   })
 
-  clearInterval(loadInterval);
-  messageDiv.innerHTML = '';
-  if(response.ok){
-    const data = await response.json();
-    const parsedData = data.bot.trim();
+  clearInterval(loadInterval)
+  messageDiv.innerHTML = ""
+  if (response.ok) {
+    const data = await response.json()
+    const parsedData = data.bot.trim()
 
-    typeText(messageDiv, parsedData);
-  }else{
-    const err = await response.text();
+    typeText(messageDiv, parsedData)
+  } else {
+    const err = await response.text()
 
-    messageDiv.innerHTML = "Something went wrong";
-    alert(err);
+    messageDiv.innerHTML = "Something went wrong"
+    alert(err)
   }
 }
 
-form.addEventListener('submit', handleSubmit);
-form.addEventListener('keyup', (e) =>{
-  if(e.keyCode === 13){
-    handleSubmit(e);
+form.addEventListener("submit", handleSubmit)
+form.addEventListener("keyup", (e) => {
+  if (e.keyCode === 13) {
+    handleSubmit(e)
   }
 })
--- server/server.js
+++ server/server.js
@@ -1,58 +1,49 @@
-import express from 'express';
-import * as dotenv from 'dotenv';
-import cors from 'cors';
-import OpenAI from "openai";
+import express from "express"
+import * as dotenv from "dotenv"
+import cors from "cors"
+import OpenAI from "openai"
 
-dotenv.config();
+dotenv.config()
 
-
-// Verify if the OPENAI_API_KEY is loaded correctly
 if (!process.env.OPENAI_API_KEY) {
-    console.error('The OPENAI_API_KEY environment variable is missing or empty.');
-    process.exit(1);
+  console.error("The OPENAI_API_KEY environment variable is missing or empty.")
+  process.exit(1)
 }
 
+const app = express()
+app.use(cors())
+app.use(express.json())
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY })
 
-const openai = new OpenAI({
-    organization: "org-HBK6BvHsyAygkQLoUZb9J2vY",
-    apiKey: process.env.OPENAI_API_KEY,
-});
+app.get("/", async (req, res) => {
+  res.status(200).send({
+    message: "Hello from MiniGPT",
+  })
+})
 
-
-
-
-
-
-app.get('/', async(req, res) => {
-    res.status(200).send({
-        message: 'Hello from MiniGPT',
+app.post("/", async (req, res) => {
+  try {
+    const prompt = req.body.prompt
+    const response = await openai.completions.create({
+      model: "gpt-3.5-turbo-instruct",
+      prompt: `${prompt}`,
+      temperature: 0,
+      max_tokens: 3000,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0,
     })
-});
-
-app.post('/', async (req, res) => {
-    try {
-        const prompt = req.body.prompt;
-        const response = await openai.create({
-            model: "gpt-3.5-turbo-instruct",
-            prompt: `${prompt}` ,
-            temperature: 0,
-            max_tokens: 3000,
-            top_p: 1,
-            frequency_penalty: 0.5,
-            presence_penalty: 0,
-        });
-        res.status(200).send({
-            bot: response.data.choices[0].text
-        });
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({error});
-        
-    }
-});
-const PORT = process.env.PORT || 5173;
-app.listen(PORT, () => console.log(`Server is running on port http://localhost:${PORT}`));
+    console.log(response)
+    res.status(200).send({
+      bot: response?.choices[0]?.text,
+    })
+  } catch (error) {
+    console.log(error)
+    res.status(500).send({ error })
+  }
+})
+const PORT = process.env.PORT || 8000
+app.listen(PORT, () =>
+  console.log(`Server is running on port http://localhost:${PORT}`)
+)
